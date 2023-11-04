cd413b67af0019826f0b14ed3e7a313c455926e9
srijagadiraju <srijasgadiraju@gmail.com>
2023-11-03 17:26:03 -0700

    updated front

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,4 +1,13 @@
 export default function App() {
+  async function testBack(){
+    console.log("testing backend"); 
+    const response = await fetch("./api/data");
+    const data = await response.json();
+    console.log(data);
+  }
+
+  testBack();
+  
   return (
     <div>
       <h1>Hello!</h1>
