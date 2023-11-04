8a60eb2e9fa31e6c629541d39173e5a7704d9048
Senay Tilahun <senay.argaw@gmail.com>
2023-11-04 14:19:08 -0700

    updates to home page

--- front/src/pages/Home.jsx
+++ front/src/pages/Home.jsx
@@ -1,13 +1,15 @@
+import { Hero, WhatVital } from "../containers";
+import { Navbar, CTA } from "../components";
+
 const Home = () => {
   return (
-    <div>
-      <h1>Home page! V3</h1>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia
-        inventore voluptates molestiae! Rem vitae dolores, quis laboriosam
-        sapiente rerum, maxime provident hic alias veritatis ipsam ipsa, impedit
-        recusandae explicabo quibusdam?
-      </p>
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Hero />
+      </div>
+      <WhatVital />
+      <CTA />
     </div>
   );
 };
