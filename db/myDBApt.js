20ffcc7faa50241d178e92912709072f790f8d47
Senay Tilahun <senay.argaw@gmail.com>
2023-11-04 19:04:58 -0700

    added db file for appointment confirmation page

---
+++ db/myDBApt.js
@@ -0,0 +1,36 @@
+import { MongoClient } from "mongodb";
+import "dotenv/config";
+
+function MyMongoDB() {
+  const myDB = {};
+
+  const uri =
+    process.env.MONGODB_URI ||
+    "mongodb+srv://user:pass@cluster0.5juizef.mongodb.net/";
+
+  function connect() {
+    const client = new MongoClient(uri);
+    const db = client.db("appointmentSharing");
+    return { client, db };
+  }
+
+  myDB.getAppointments = async function (query = {}) {
+    const { client, db } = connect();
+
+    try {
+      const appointments = await db
+        .collection("appointments")
+        .find(query)
+        .toArray();
+      return appointments;
+    } finally {
+      await client.close();
+    }
+  };
+
+  return myDB;
+}
+
+const myDB = MyMongoDB();
+
+export default myDB;
