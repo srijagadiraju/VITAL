da820ded65dfc341323c53cfcd805c2c4da80bb7
srijagadiraju <srijasgadiraju@gmail.com>
2023-11-05 17:52:03 -0800

    patient portal updated with search and connection to confirmation page

--- front/src/components/portal/Portal.jsx
+++ front/src/components/portal/Portal.jsx
@@ -2,11 +2,12 @@ import React, { useState, useEffect } from "react";
 import PropTypes from 'prop-types';
 import "./portal.css";
 import SearchBar from "../searchBar/SearchBar";
+import { useNavigate } from "react-router-dom";
 
 const Portal = ({ appointments }) => {
-    const [openMenu, setOpenMenu] = useState(false);
     const [appointmentsData, setAppointmentsData] = useState([]);
     const [query, setQuery] = useState("");
+    const navigate = useNavigate();
 
     useEffect(() => {
         async function fetchData() {
@@ -37,6 +38,10 @@ const Portal = ({ appointments }) => {
         );
     });
 
+    const handleSelect = () => {
+        navigate('/apt-confirmation/1');
+    };
+
     return (
         <div className="container">
             <h1>Welcome to the Appointment Portal!</h1>
@@ -50,7 +55,7 @@ const Portal = ({ appointments }) => {
                                 <p className="card-text"><strong>Department:</strong> {appointment.department}</p>
                                 <p className="card-text"><strong>Visit:</strong> {appointment.visit}</p>
                                 <p className="card-text"><strong>Time:</strong> {appointment.time}</p>
-                                <button onClick={() => handleSelect(appointment)}>Select</button>
+                                <button onClick={handleSelect}>Select This Appointment</button>
                             </div>
                         </div>
                     </div>
@@ -72,64 +77,3 @@ Portal.propTypes = {
 };
 
 export default Portal;
-
-
-// const Portal = ({ appointments }) => {
-//   const [openMenu, setOpenMenu] = useState(false);
-//   const [appointmentsData, setAppointmentsData] = useState([]);
-//   const [query, setQuery] = useState(""); 
-
-//   useEffect(() => {
-//     async function fetchData() {
-//       try {
-//         const response = await fetch("/api/appointments");
-//         const data = await response.json();
-//         const formattedAppointments = data.map(appointment => ({
-//           name: appointment["Doctor's Name"],
-//           department: appointment.Department,
-//           visit: appointment.Visit,
-//           time: appointment["Time of Appointment"]
-//         }));
-//         setAppointmentsData(formattedAppointments);
-//       } catch (error) {
-//         console.error('Error fetching appointments:', error);
-//       }
-//     }
-//     fetchData();
-//   }, []);
-
-//   return (
-//     <div className="container">
-//       <h1>Welcome to the Appointment Portal!</h1>
-//       <SearchBar query={query} setQuery={setQuery}/>
-//       <div className="row">
-//         {appointmentsData.map((appointment, index) => (
-//           <div key={index} className="col-4">
-//             <div className="card">
-//               <div className="card-body">
-//                 <h5 className="card-title">Name: {appointment.name}</h5>
-//                 <p className="card-text"><strong>Department:</strong> {appointment.department}</p>
-//                 <p className="card-text"><strong>Visit:</strong> {appointment.visit}</p>
-//                 <p className="card-text"><strong>Time:</strong> {appointment.time}</p>
-//                 <button onClick={() => handleSelect(appointment)}>Select</button>
-//               </div>
-//             </div>
-//           </div>
-//         ))}
-//       </div>
-//     </div>
-//   );
-// };
-
-// Portal.propTypes = {
-//   appointments: PropTypes.arrayOf(
-//     PropTypes.shape({
-//       name: PropTypes.string.isRequired,
-//       department: PropTypes.string.isRequired,
-//       visit: PropTypes.string.isRequired,
-//       time: PropTypes.string.isRequired
-//     })
-//   )
-// };
-
-// export default Portal;
