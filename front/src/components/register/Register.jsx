e3043fdc2cc63340d76f59272246bc8578103574
srijagadiraju <srijasgadiraju@gmail.com>
2023-12-09 11:51:00 -0800

    updated button contrast for register and log in page

--- front/src/components/register/Register.jsx
+++ front/src/components/register/Register.jsx
@@ -1,80 +1,3 @@
-// import React, { useState } from "react";
-// import { useNavigate } from "react-router-dom";
-// import "./register.css";
-
-// const Register = () => {
-//   const [email, setEmail] = useState("");
-//   const [username, setUsername] = useState("");
-//   const [password, setPassword] = useState("");
-//   const [showSuccess, setShowSuccess] = useState(false);
-//   const navigate = useNavigate();
-
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-
-//     // check if all fields are filled
-//     if (
-//       email.trim() === "" ||
-//       username.trim() === "" ||
-//       password.trim() === ""
-//     ) {
-//       alert("Please fill in all fields to register.");
-//     } else {
-//       setTimeout(() => {
-//         setShowSuccess(true);
-
-//         setTimeout(() => {
-//           navigate("/join");
-//         }, 2000);
-//       }, 2000);
-//     }
-//   };
-
-//   return (
-//     <div className="container">
-//       <h1>Register</h1>
-//       <form onSubmit={handleSubmit}>
-//         <div className="form-group">
-//           <label htmlFor="email">Email</label>
-//           <input
-//             type="email"
-//             id="email"
-//             value={email}
-//             onChange={(e) => setEmail(e.target.value)}
-//           />
-//         </div>
-//         <div className="form-group">
-//           <label htmlFor="username">Username</label>
-//           <input
-//             type="text"
-//             id="username"
-//             value={username}
-//             onChange={(e) => setUsername(e.target.value)}
-//           />
-//         </div>
-//         <div className="form-group">
-//           <label htmlFor="password">Password</label>
-//           <input
-//             type="password"
-//             id="password"
-//             value={password}
-//             onChange={(e) => setPassword(e.target.value)}
-//           />
-//         </div>
-//         <button type="submit">Sign Up</button>
-//       </form>
-
-//       {showSuccess && (
-//         <div className="success-popup">
-//           <p>You have registered successfully!</p>
-//         </div>
-//       )}
-//     </div>
-//   );
-// };
-
-// export default Register;
-
 import { useState, useEffect } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import "./register.css";
@@ -97,7 +20,7 @@ const Register = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    // Check if all fields are filled
+    // check if all fields are filled
     if (
       email.trim() === "" ||
       username.trim() === "" ||
@@ -105,11 +28,11 @@ const Register = () => {
     ) {
       alert("Please fill in all fields to register.");
     } else {
-      // Simulate registration process
+      // simulate registration process
       setTimeout(() => {
         setShowSuccess(true);
 
-        // Navigate to the join page after success
+        // navigate to the join page after success
         setTimeout(() => {
           navigate("/join");
         }, 2000);
